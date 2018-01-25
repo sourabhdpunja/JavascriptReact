c7b0e898d55115e3c1e3d0f8bcd68b284f208df3
Sourabh Punja <spunja@cogitocorp.com>
2018-01-24 19:12:46 -0500

    Redux code

---
+++ Redux/ReduxSimpleStarter/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function(){
+    return [
+        {title: 'Javascript: The Good Parts'},
+        {title: 'New Product'},
+        {title: 'Javascript: The Good Parts 2'},
+        {title: 'Javascript: The Good Parts 3'}
+    ]
+}
