e40b07e3b2894175dd4a4642107a6c1235e92396
Sourabh Punja <spunja@cogitocorp.com>
2018-01-29 10:05:21 -0500

    First work

--- Redux/ReduxSimpleStarter/src/components/app.js
+++ Redux/ReduxSimpleStarter/src/components/app.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
-
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import FlatButton from 'material-ui/FlatButton';
+import RaisedButton from 'material-ui/RaisedButton';
 import BookList from '../containers/book-list';
 import BookDetail from '../containers/book-detail';
+
+const style = {
+  margin: 12,
+};
 
 export default class App extends Component {
   render() {
@@ -9,6 +17,12 @@ export default class App extends Component {
       <div>
         <BookList/>
         <BookDetail/>
+        <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+            <div>
+              <FlatButton label="Default" />
+              <RaisedButton label="Primary" primary={true} style={style} />
+             </div>
+        </MuiThemeProvider>
       </div>
     );
   }
